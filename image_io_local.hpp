2447149e6869b49e9d27c45bc571da34924ad803
OSAMU WATANABE <owatanab@es.takushoku-u.ac.jp>
2022-03-04 14:43:31 +0900

    rewrite

--- image_io_local.hpp
+++ image_io_local.hpp
@@ -7,6 +7,11 @@
 #include <string>
 #include <vector>
 
+#if defined(__ARM_NEON__) || defined(__ARM64__)
+  #define USE_ARM_NEON
+  #include <arm_neon.h>
+#endif
+
 constexpr char SP = ' ';
 constexpr char LF = '\n';
 constexpr char CR = 0x0d;
@@ -74,3 +79,29 @@ class image_component {
   }
   auto move_buf() { return std::move(buf); }
 };
+
+#if defined(USE_ARM_NEON)
+// store uint8x16_t vector as int32
+auto store_u8_to_s32 = [](uint8x16_t &src, int32_t *dst) {
+  int16x8_t l = vreinterpretq_s16_u16(vmovl_u8(vget_low_u8(src)));
+  int16x8_t h = vreinterpretq_s16_u16(vmovl_u8(vget_high_u8(src)));
+  auto ll     = vmovl_s16(vget_low_s16(l));
+  auto lh     = vmovl_s16(vget_high_s16(l));
+  auto hl     = vmovl_s16(vget_low_s16(h));
+  auto hh     = vmovl_s16(vget_high_s16(h));
+  vst1q_s32(dst, ll);
+  vst1q_s32(dst + 4, lh);
+  vst1q_s32(dst + 8, hl);
+  vst1q_s32(dst + 12, hh);
+};
+
+// store uint16x8_t vector as int32
+auto store_u16_to_s32 = [](uint16x8_t &src, int32_t *dst) {
+  auto little_big = vrev16q_u8(src);  // convert endianness from big to little
+  auto x0         = vreinterpretq_s32_s16(little_big);
+  auto xl         = vmovl_s16(vget_low_s32(x0));
+  auto xh         = vmovl_s16(vget_high_s32(x0));
+  vst1q_s32(dst, xl);
+  vst1q_s32(dst + 4, xh);
+};
+#endif
