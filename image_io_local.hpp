bbfb13b87248170262f5be06c593f6dbe390201b
OSAMU WATANABE <owatanab@es.takushoku-u.ac.jp>
2022-04-21 15:28:31 +0900

    msvc

--- image_io_local.hpp
+++ image_io_local.hpp
@@ -10,9 +10,13 @@
 #if defined(__ARM_NEON__) || defined(__ARM_NEON)
   #define USE_ARM_NEON
   #include <arm_neon.h>
-#elif defined(__AVX2__)
+#elif defined(__AVX2__) || defined(__MINGW64__)
   #define USEAVX2
-  #include <x86intrin.h>
+  #if defined(_MSC_VER)
+    #include <intrin.h>
+  #else
+    #include <x86intrin.h>
+  #endif
 #endif
 
 constexpr char SP = ' ';
@@ -34,15 +38,27 @@ static auto eat_white = [](int &d, FILE *fp, char *comment) {
 
 static inline void *aligned_mem_alloc(size_t size, size_t align) {
   void *result;
+#if defined(_MSC_VER)
+  result = _aligned_malloc(size, align);
+#elif defined(__MINGW32__) || defined(__MINGW64__)
+  result = __mingw_aligned_malloc(size, align);
+#else
   if (posix_memalign(&result, align, size)) {
     result = nullptr;
   }
+#endif
   return result;
 }
 
 template <class T>
 struct delete_aligned {
+#if defined(_MSC_VER)
+  _aligned_free(data);
+#elif defined(__MINGW32__) || defined(__MINGW64__)
+  __mingw_aligned_free(data);
+#else
   void operator()(T *data) const { free(data); }
+#endif
 };
 template <class T>
 using unique_ptr_aligned = std::unique_ptr<T, delete_aligned<T>>;
@@ -167,37 +183,37 @@ auto load_u8_store_s32 = [](uint8_t const *src, int32_t *const R, int32_t *const
   tmp3 = _mm_alignr_epi8(tmp1, tmp3, 10);  // a:0,3,6,9,12,15,b:2,5,8,11,14,x,x,x,x,x
   tmp3 = _mm_slli_si128(tmp3, 5);          // 0,0,0,0,0,a:0,3,6,9,12,15,b:2,5,8,11,14,
   tmp3 = _mm_srli_si128(tmp3, 5);          // a:0,3,6,9,12,15,b:2,5,8,11,14,:0,0,0,0,0
-  v0   = _mm_slli_si128(tmp2, 11);         // 0,0,0,0,0,0,0,0,0,0,0,0, 1,4,7,10,13,
-  v0   = _mm_or_si128(v0, tmp3);           // a:0,3,6,9,12,15,b:2,5,8,11,14,c:1,4,7,10,13,
+  v0 = _mm_slli_si128(tmp2, 11);           // 0,0,0,0,0,0,0,0,0,0,0,0, 1,4,7,10,13,
+  v0 = _mm_or_si128(v0, tmp3);             // a:0,3,6,9,12,15,b:2,5,8,11,14,c:1,4,7,10,13,
 
   tmp3 = _mm_slli_si128(tmp0, 5);   // 0,0,0,0,0,a:0,3,6,9,12,15,1,4,7,10,13,
   tmp3 = _mm_srli_si128(tmp3, 11);  // a:1,4,7,10,13, 0,0,0,0,0,0,0,0,0,0,0
-  v1   = _mm_srli_si128(tmp1, 5);   // b:0,3,6,9,12,15,C:1,4,7,10,13, 0,0,0,0,0
-  v1   = _mm_slli_si128(v1, 5);     // 0,0,0,0,0,b:0,3,6,9,12,15,C:1,4,7,10,13,
-  v1   = _mm_or_si128(v1, tmp3);    // a:1,4,7,10,13,b:0,3,6,9,12,15,C:1,4,7,10,13,
-  v1   = _mm_slli_si128(v1, 5);     // 0,0,0,0,0,a:1,4,7,10,13,b:0,3,6,9,12,15,
-  v1   = _mm_srli_si128(v1, 5);     // a:1,4,7,10,13,b:0,3,6,9,12,15,0,0,0,0,0
+  v1 = _mm_srli_si128(tmp1, 5);     // b:0,3,6,9,12,15,C:1,4,7,10,13, 0,0,0,0,0
+  v1 = _mm_slli_si128(v1, 5);       // 0,0,0,0,0,b:0,3,6,9,12,15,C:1,4,7,10,13,
+  v1 = _mm_or_si128(v1, tmp3);      // a:1,4,7,10,13,b:0,3,6,9,12,15,C:1,4,7,10,13,
+  v1 = _mm_slli_si128(v1, 5);       // 0,0,0,0,0,a:1,4,7,10,13,b:0,3,6,9,12,15,
+  v1 = _mm_srli_si128(v1, 5);       // a:1,4,7,10,13,b:0,3,6,9,12,15,0,0,0,0,0
   tmp3 = _mm_srli_si128(tmp2, 5);   // c:2,5,8,11,14,0,3,6,9,12,15,0,0,0,0,0
   tmp3 = _mm_slli_si128(tmp3, 11);  // 0,0,0,0,0,0,0,0,0,0,0,c:2,5,8,11,14,
-  v1   = _mm_or_si128(v1, tmp3);    // a:1,4,7,10,13,b:0,3,6,9,12,15,c:2,5,8,11,14,
+  v1 = _mm_or_si128(v1, tmp3);      // a:1,4,7,10,13,b:0,3,6,9,12,15,c:2,5,8,11,14,
 
-  tmp3       = _mm_srli_si128(tmp2, 10);  // c:0,3,6,9,12,15, 0,0,0,0,0,0,0,0,0,0,
-  tmp3       = _mm_slli_si128(tmp3, 10);  // 0,0,0,0,0,0,0,0,0,0, c:0,3,6,9,12,15,
-  v2         = _mm_srli_si128(tmp1, 11);  // b:1,4,7,10,13,0,0,0,0,0,0,0,0,0,0,0
-  v2         = _mm_slli_si128(v2, 5);     // 0,0,0,0,0,b:1,4,7,10,13, 0,0,0,0,0,0
-  v2         = _mm_or_si128(v2, tmp3);    // 0,0,0,0,0,b:1,4,7,10,13,c:0,3,6,9,12,15,
-  tmp0       = _mm_srli_si128(tmp0, 11);  // a:2,5,8,11,14, 0,0,0,0,0,0,0,0,0,0,0,
-  v2         = _mm_or_si128(v2, tmp0);    // a:2,5,8,11,14,b:1,4,7,10,13,c:0,3,6,9,12,15,
+  tmp3 = _mm_srli_si128(tmp2, 10);  // c:0,3,6,9,12,15, 0,0,0,0,0,0,0,0,0,0,
+  tmp3 = _mm_slli_si128(tmp3, 10);  // 0,0,0,0,0,0,0,0,0,0, c:0,3,6,9,12,15,
+  v2 = _mm_srli_si128(tmp1, 11);    // b:1,4,7,10,13,0,0,0,0,0,0,0,0,0,0,0
+  v2 = _mm_slli_si128(v2, 5);       // 0,0,0,0,0,b:1,4,7,10,13, 0,0,0,0,0,0
+  v2 = _mm_or_si128(v2, tmp3);      // 0,0,0,0,0,b:1,4,7,10,13,c:0,3,6,9,12,15,
+  tmp0 = _mm_srli_si128(tmp0, 11);  // a:2,5,8,11,14, 0,0,0,0,0,0,0,0,0,0,0,
+  v2 = _mm_or_si128(v2, tmp0);      // a:2,5,8,11,14,b:1,4,7,10,13,c:0,3,6,9,12,15,
   auto Rhigh = _mm_srli_si128(v0, 8);
-  auto Rlow  = _mm_move_epi64(v0);
+  auto Rlow = _mm_move_epi64(v0);
   _mm256_storeu_si256((__m256i *)R, _mm256_cvtepi8_epi32(Rlow));
   _mm256_storeu_si256((__m256i *)(R + 8), _mm256_cvtepi8_epi32(Rhigh));
   auto Ghigh = _mm_srli_si128(v1, 8);
-  auto Glow  = _mm_move_epi64(v1);
+  auto Glow = _mm_move_epi64(v1);
   _mm256_storeu_si256((__m256i *)G, _mm256_cvtepi8_epi32(Glow));
   _mm256_storeu_si256((__m256i *)(G + 8), _mm256_cvtepi8_epi32(Ghigh));
   auto Bhigh = _mm_srli_si128(v2, 8);
-  auto Blow  = _mm_move_epi64(v2);
+  auto Blow = _mm_move_epi64(v2);
   _mm256_storeu_si256((__m256i *)B, _mm256_cvtepi8_epi32(Blow));
   _mm256_storeu_si256((__m256i *)(B + 8), _mm256_cvtepi8_epi32(Bhigh));
 };
@@ -207,51 +223,51 @@ auto load_u16_store_s32 = [](uint16_t const *src, int32_t *const R, int32_t *con
   alignas(16) static const int8_t mask16_0[16] = {0, 1, 6, 7, 12, 13, 2, 3, 8, 9, 14, 15, 4, 5, 10, 11};
   alignas(16) static const int8_t mask16_1[16] = {2, 3, 8, 9, 14, 15, 4, 5, 10, 11, 0, 1, 6, 7, 12, 13};
   alignas(16) static const int8_t mask16_2[16] = {4, 5, 10, 11, 0, 1, 6, 7, 12, 13, 2, 3, 8, 9, 14, 15};
-  alignas(16) static const int8_t count8[2]    = {8, 8};
-  __m128i v0                                   = _mm_loadu_si128((__m128i *)(src));  // a0,a1,a2,a3,...a7,
-  __m128i v1                                   = _mm_loadu_si128((__m128i *)(src + 8));  // b0,b1,b2,b3...b7
+  alignas(16) static const int8_t count8[2] = {8, 8};
+  __m128i v0 = _mm_loadu_si128((__m128i *)(src));       // a0,a1,a2,a3,...a7,
+  __m128i v1 = _mm_loadu_si128((__m128i *)(src + 8));   // b0,b1,b2,b3...b7
   __m128i v2 = _mm_loadu_si128((__m128i *)(src + 16));  // c0,c1,c2,c3,...c7
 
   tmp0 = _mm_shuffle_epi8(v0, *(__m128i *)mask16_0);  // a0,a3,a6,a1,a4,a7,a2,a5,
   tmp1 = _mm_shuffle_epi8(v1, *(__m128i *)mask16_1);  // b1,b4,b7,b2,b5,b0,b3,b6
   tmp2 = _mm_shuffle_epi8(v2, *(__m128i *)mask16_2);  // c2,c5, c0,c3,c6, c1,c4,c7
 
-  tmp3        = _mm_slli_si128(tmp0, 10);         // 0,0,0,0,0,a0,a3,a6,
-  tmp3        = _mm_alignr_epi8(tmp1, tmp3, 10);  // a0,a3,a6,b1,b4,b7,x,x
-  tmp3        = _mm_slli_si128(tmp3, 4);          // 0,0, a0,a3,a6,b1,b4,b7
-  tmp3        = _mm_srli_si128(tmp3, 4);          // a0,a3,a6,b1,b4,b7,0,0
-  v0          = _mm_slli_si128(tmp2, 12);         // 0,0,0,0,0,0, c2,c5,
-  v0          = _mm_or_si128(v0, tmp3);           // a0,a3,a6,b1,b4,b7,c2,c5
+  tmp3 = _mm_slli_si128(tmp0, 10);         // 0,0,0,0,0,a0,a3,a6,
+  tmp3 = _mm_alignr_epi8(tmp1, tmp3, 10);  // a0,a3,a6,b1,b4,b7,x,x
+  tmp3 = _mm_slli_si128(tmp3, 4);          // 0,0, a0,a3,a6,b1,b4,b7
+  tmp3 = _mm_srli_si128(tmp3, 4);          // a0,a3,a6,b1,b4,b7,0,0
+  v0 = _mm_slli_si128(tmp2, 12);           // 0,0,0,0,0,0, c2,c5,
+  v0 = _mm_or_si128(v0, tmp3);             // a0,a3,a6,b1,b4,b7,c2,c5
   __m128i v0a = _mm_slli_epi16(v0, 8);
   __m128i v0b = _mm_srli_epi16(v0, 8);
-  v0          = _mm_or_si128(v0a, v0b);
+  v0 = _mm_or_si128(v0a, v0b);
   _mm256_stream_si256((__m256i *)R, _mm256_cvtepu16_epi32(v0));
 
-  tmp3        = _mm_slli_si128(tmp0, 4);   // 0,0,a0,a3,a6,a1,a4,a7
-  tmp3        = _mm_srli_si128(tmp3, 10);  // a1,a4,a7, 0,0,0,0,0
-  v1          = _mm_srli_si128(tmp1, 6);   // b2,b5,b0,b3,b6,0,0
-  v1          = _mm_slli_si128(v1, 6);     // 0,0,0,b2,b5,b0,b3,b6,
-  v1          = _mm_or_si128(v1, tmp3);    // a1,a4,a7,b2,b5,b0,b3,b6,
-  v1          = _mm_slli_si128(v1, 6);     // 0,0,0,a1,a4,a7,b2,b5,
-  v1          = _mm_srli_si128(v1, 6);     // a1,a4,a7,b2,b5,0,0,0,
-  tmp3        = _mm_srli_si128(tmp2, 4);   // c0,c3,c6, c1,c4,c7,0,0
-  tmp3        = _mm_slli_si128(tmp3, 10);  // 0,0,0,0,0,c0,c3,c6,
-  v1          = _mm_or_si128(v1, tmp3);    // a1,a4,a7,b2,b5,c0,c3,c6,
+  tmp3 = _mm_slli_si128(tmp0, 4);   // 0,0,a0,a3,a6,a1,a4,a7
+  tmp3 = _mm_srli_si128(tmp3, 10);  // a1,a4,a7, 0,0,0,0,0
+  v1 = _mm_srli_si128(tmp1, 6);     // b2,b5,b0,b3,b6,0,0
+  v1 = _mm_slli_si128(v1, 6);       // 0,0,0,b2,b5,b0,b3,b6,
+  v1 = _mm_or_si128(v1, tmp3);      // a1,a4,a7,b2,b5,b0,b3,b6,
+  v1 = _mm_slli_si128(v1, 6);       // 0,0,0,a1,a4,a7,b2,b5,
+  v1 = _mm_srli_si128(v1, 6);       // a1,a4,a7,b2,b5,0,0,0,
+  tmp3 = _mm_srli_si128(tmp2, 4);   // c0,c3,c6, c1,c4,c7,0,0
+  tmp3 = _mm_slli_si128(tmp3, 10);  // 0,0,0,0,0,c0,c3,c6,
+  v1 = _mm_or_si128(v1, tmp3);      // a1,a4,a7,b2,b5,c0,c3,c6,
   __m128i v1a = _mm_slli_epi16(v1, 8);
   __m128i v1b = _mm_srli_epi16(v1, 8);
-  v1          = _mm_or_si128(v1a, v1b);
+  v1 = _mm_or_si128(v1a, v1b);
   _mm256_stream_si256((__m256i *)G, _mm256_cvtepu16_epi32(v1));
 
-  tmp3        = _mm_srli_si128(tmp2, 10);  // c1,c4,c7, 0,0,0,0,0
-  tmp3        = _mm_slli_si128(tmp3, 10);  // 0,0,0,0,0, c1,c4,c7,
-  v2          = _mm_srli_si128(tmp1, 10);  // b0,b3,b6,0,0, 0,0,0
-  v2          = _mm_slli_si128(v2, 4);     // 0,0, b0,b3,b6,0,0,0
-  v2          = _mm_or_si128(v2, tmp3);    // 0,0, b0,b3,b6,c1,c4,c7,
-  tmp0        = _mm_srli_si128(tmp0, 12);  // a2,a5,0,0,0,0,0,0
-  v2          = _mm_or_si128(v2, tmp0);    // a2,a5,b0,b3,b6,c1,c4,c7,
+  tmp3 = _mm_srli_si128(tmp2, 10);  // c1,c4,c7, 0,0,0,0,0
+  tmp3 = _mm_slli_si128(tmp3, 10);  // 0,0,0,0,0, c1,c4,c7,
+  v2 = _mm_srli_si128(tmp1, 10);    // b0,b3,b6,0,0, 0,0,0
+  v2 = _mm_slli_si128(v2, 4);       // 0,0, b0,b3,b6,0,0,0
+  v2 = _mm_or_si128(v2, tmp3);      // 0,0, b0,b3,b6,c1,c4,c7,
+  tmp0 = _mm_srli_si128(tmp0, 12);  // a2,a5,0,0,0,0,0,0
+  v2 = _mm_or_si128(v2, tmp0);      // a2,a5,b0,b3,b6,c1,c4,c7,
   __m128i v2a = _mm_slli_epi16(v2, 8);
   __m128i v2b = _mm_srli_epi16(v2, 8);
-  v2          = _mm_or_si128(v2a, v2b);
+  v2 = _mm_or_si128(v2a, v2b);
   _mm256_stream_si256((__m256i *)B, _mm256_cvtepu16_epi32(v2));
 };
 #endif
